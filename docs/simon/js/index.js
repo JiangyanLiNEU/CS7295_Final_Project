1d27680b45d08111d07ee4ce5e86d5e457c135ef
Fengrui Gan <fengruigan@gmail.com>
2023-04-23 00:00:36 -0700

    finish up simon's scrolly-telling

--- docs/simon/js/index.js
+++ docs/simon/js/index.js
@@ -2,29 +2,67 @@
 import VisController from "./VisController.js";
 
 const scroller = scrollama();
-const { overallUr, overallIr } = VisController;
+const { overallUr, overallIr, alignedComparison, ethnicityUr, ethnicityUrSetIndexDate, renderUrMap } = VisController;
 
 // const steps = document.querySelectorAll(".step");
 const fadeTime = 5;
 
-document.querySelector("#toggle").addEventListener("click", () => {
-  toggle();
+// Start action
+document.querySelector("#startButton").addEventListener("click", () => {
+  document.querySelector("#overview").scrollIntoView({ behavior: "smooth" });
+});
+
+// ur-by-ethnicity interactions
+document.querySelector("#endOfGR").addEventListener("click", () => {
+  ethnicityUrSetIndexDate(new Date("6/1/2009"));
+});
+document.querySelector("#endOfCR").addEventListener("click", () => {
+  ethnicityUrSetIndexDate(new Date("4/1/2020"));
 });
 
 const cbs = [
-  { cb: overallUr, args: [false] },
-  { cb: overallUr, args: [true] },
-  { cb: overallIr, args: [false] },
-  { cb: overallIr, args: [true] },
+  { cb: overallUr, args: [false, { "#bottomNote1": "Drag and scroll to zoom select custom date range." }] },
+  { cb: overallUr, args: [true, { "#bottomNote1": "Drag and scroll to zoom select custom date range." }] },
+  { cb: overallIr, args: [false, { "#bottomNote1": "Drag and scroll to zoom select custom date range." }] },
+  { cb: overallIr, args: [true, { "#bottomNote1": "Drag and scroll to zoom select custom date range." }] },
+  { cb: alignedComparison, args: ["end", false, { "#bottomNote2": "Mouse over the lines to highlight." }] },
+  { cb: alignedComparison, args: ["start", false, { "#bottomNote2": "Mouse over the lines to highlight." }] },
+  { cb: alignedComparison, args: ["start", true, { "#bottomNote2": "Mouse over the lines to highlight." }] },
+  { cb: ethnicityUr, args: [false, {}] },
+  { cb: ethnicityUr, args: [true, { "#bottomNote3": "Mouse over to index by a different date" }] },
+  {
+    cb: renderUrMap,
+    args: [
+      "Great Recession (2007 - 09)",
+      "COVID-19 Recession (2020)",
+      false,
+      { "#bottomNote4": "Mouse over and scroll to zoom in" },
+    ],
+  },
+  {
+    cb: renderUrMap,
+    args: [
+      "Great Recession (2007 - 09)",
+      "Dot-Bomb Recession (2001)",
+      false,
+      { "#bottomNote4": "Mouse over and scroll to zoom in" },
+    ],
+  },
+  {
+    cb: renderUrMap,
+    args: [
+      "Great Recession (2007 - 09)",
+      "Dot-Bomb Recession (2001)",
+      true,
+      { "#bottomNote4": "Mouse over and scroll to zoom in" },
+    ],
+  },
 ];
 
 function fadeOut(element) {
   let op = 1; // initial opacity
   let timer = setInterval(() => {
-    if (op <= 0.1) {
-      clearInterval(timer);
-      //   element.style.display = "none";
-    }
+    if (op <= 0.1) clearInterval(timer);
     element.style.opacity = op;
     element.style.filter = "alpha(opacity=" + op * 100 + ")";
     op -= op * 0.1;
@@ -33,7 +71,6 @@ function fadeOut(element) {
 
 function fadeIn(element) {
   let op = 0.1; // initial opacity
-  //   element.style.display = "block";
   let timer = setInterval(() => {
     if (op >= 1) {
       clearInterval(timer);
